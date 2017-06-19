2eed2e7e0563334b6816103de33b299f625611bd
夏宇霆 <56833517@qq.com>
2017-06-19 15:38:23 +0800

    server

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 rm -fr ./node_modules
 node --version
+npm --version
 npm install -g cnpm --registry=https://registry.npm.taobao.org
-cnpm install -g diff-so-fancy npm-check
+cnpm install -g diff-so-fancy npm-check live-server shelljs debug yargs
 cnpm install
+npm link
