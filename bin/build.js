0b07b3f4f3f981fd0f4face01bc9e6f2bbd8561d
夏宇霆 <56833517@qq.com>
2017-06-19 17:06:36 +0800

    build is npm --var=something run var 处理npm传给node传给bin

--- bin/build.js
+++ bin/build.js
@@ -8,4 +8,8 @@ shell.exec("echo hello " + name);
 var child = exec('echo hello ' + name, function(err, stdout, stderr) {
   if (err) throw err;
   console.log(stdout);
+  console.log({ var: process.env.npm_config_var })
 });
+
+
+// npm --var=something run var
