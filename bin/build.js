2381f5ba9304d001238f2d7749799fef8b8b74f6
夏宇霆 <56833517@qq.com>
2017-06-19 17:49:10 +0800

    build is npm --var=something run var 处理npm传给node传给bin

--- bin/build.js
+++ bin/build.js
@@ -2,15 +2,16 @@
 const name = process.argv[2]
 const exec = require('child_process').exec
 const shell = require("shelljs")
-const debug = require('debug')('app:bin:build')
 
 shell.exec("echo hello " + name)
 
 const child = exec('echo hello ' + name, function(err, stdout, stderr) {
   if (err) throw err
-  debug(stdout)
-  debug({ ip: process.env.npm_config_ip })
+  console.log(stdout)
+  console.log({ ip: process.env.npm_config_ip })
 })
 
+// build world
 
-// npm --ip=something run ip
+// 不输出npm命令头
+// npm -s --ip=192.168.0.140:9090 run ip
