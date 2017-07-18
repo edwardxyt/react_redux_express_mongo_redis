55b5edaaa1783643412081377e25da50578716aa
夏宇霆 <56833517@qq.com>
2017-07-18 10:53:38 +0800

    静态文件交给nginx

--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@ rm -fr ./node_modules
 node --version
 npm --version
 npm install -g cnpm --registry=https://registry.npm.taobao.org
-cnpm install -g diff-so-fancy npm-check live-server shelljs debug yargs jsdoc
+cnpm install -g diff-so-fancy npm-check live-server shelljs debug yargs jsdoc pm2
 cnpm install
 npm link
