71233c5b3d87fc0024004d1b8664532d35de3ab8
夏宇霆 <56833517@qq.com>
2017-10-18 16:57:01 +0800

    BUG的gateway

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 rm -fr ./node_modules
 node --version
 npm --version
+npm i -g npm to update
 npm install -g cnpm --registry=https://registry.npm.taobao.org
-cnpm install -g diff-so-fancy npm-check live-server shelljs debug yargs jsdoc gtop pm2
-cnpm install
+cnpm install -g better-npm-run vue-cli diff-so-fancy npm-check live-server shelljs debug yargs jsdoc gtop pm2
 npm link
