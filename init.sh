34eb641f6a48a9f67d15a6c7f54fbfd19393c38a
夏宇霆 <56833517@qq.com>
2017-10-30 17:15:47 +0800

    create-react-app

--- init.sh
+++ init.sh
@@ -3,5 +3,5 @@ node --version
 npm --version
 npm i -g npm to update
 npm install -g cnpm --registry=https://registry.npm.taobao.org
-cnpm install -g better-npm-run vue-cli diff-so-fancy npm-check live-server shelljs debug yargs jsdoc gtop pm2
+cnpm install -g create-react-app detect-port nodemon glob fs-extra rimraf cross-env better-npm-run vue-cli diff-so-fancy npm-check live-server shelljs debug yargs jsdoc gtop pm2
 npm link
