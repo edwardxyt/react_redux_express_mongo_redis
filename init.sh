e0f37dbf16c23ea7147eff3b8a71836ca74dcf6e
夏宇霆 <56833517@qq.com>
2018-03-19 10:38:49 +0800

    sh

--- init.sh
+++ init.sh
@@ -3,5 +3,6 @@ node --version
 npm --version
 npm i -g npm to update
 npm install -g cnpm --registry=https://registry.npm.taobao.org
-cnpm install -g create-react-app detect-port nodemon glob fs-extra rimraf cross-env better-npm-run vue-cli diff-so-fancy npm-check live-server shelljs debug yargs jsdoc gtop pm2
+cnpm install -g json-server detect-port fkill-cli nodemon glob fs-extra rimraf cross-env better-npm-run vue-cli weex-toolkit diff-so-fancy npm-check live-server shelljs debug yargs jsdoc gtop pm2
+git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
 npm link
